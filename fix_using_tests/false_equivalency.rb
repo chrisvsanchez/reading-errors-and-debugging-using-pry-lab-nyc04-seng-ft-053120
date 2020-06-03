c7a0b7624e6558f09b09a27d7d791388f4140f32
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-03 02:14:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,6 @@
 # don't forget to add: require 'pry'
 require 'pry'
+
 def get_user_input
   gets.chomp
 end
@@ -16,6 +17,7 @@ def selection(num)
   elsif num = 2
      puts "HAM HAM HAM IN MY TUMMY"
   end
+  binding.pry
 end
 
 def runner
